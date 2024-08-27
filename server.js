d1262341fd173d94512edafea3c930db952398d4
Parfene Daniel <parfenedaniel345@gmail.com>
2024-08-27 22:48:05 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  const ip =
+    req.headers["cf-connecting-ip"] ||
+    req.headers["x-real-ip"] ||
+    req.headers["x-forwarded-for"] ||
+    request.socket.remoteAddress ||
+    "";
+
+  return res.json({ ip });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running");
+});
