0fd19837c97ac9333597123f552a93a2551fb50d
Parfene Daniel <parfenedaniel345@gmail.com>
2024-09-12 18:45:51 +0300

    uuu

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { rateLimit } = require("express-rate-limit");
+const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
   windowMs: 2 * 60 * 1000,
