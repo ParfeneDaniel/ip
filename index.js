a6ad49c330d8362a033ab99294c2fcb82d70bee6
Parfene Daniel <parfenedaniel345@gmail.com>
2024-09-12 18:29:22 +0300

    up

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   try {
-    const IP = req.socket.remoteAddress;
-    return res.status(200).json({ IP });
+    const ip =
+      req.headers["cf-connecting-ip"] ||
+      req.headers["x-real-ip"] ||
+      req.headers["x-forwarded-for"] ||
+      req.socket.remoteAddress ||
+      "";
+    return res.status(200).json({ ip });
   } catch (error) {
     return res.status(500).json({ message: "Internal Server Error" });
   }
