104490e41cd3af57f156626883b4311b8e8d97ae
Parfene Daniel <parfenedaniel345@gmail.com>
2024-09-12 13:56:34 +0300

    update

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const express = require("express");
+const { rateLimit } = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 3 * 60 * 1000,
+  limit: 5,
+});
 
 const app = express();
 app.use(express.json());
+app.use(limiter);
 
 app.get("/", (req, res) => {
-  const ip =
-    req.headers["cf-connecting-ip"] ||
-    req.headers["x-real-ip"] ||
-    req.headers["x-forwarded-for"] ||
-    req.socket.remoteAddress ||
-    "";
-
-  return res.json({ ip });
+  try {
+    const IP = req.socket.remoteAddress;
+    return res.status(200).json({ IP });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 module.exports = app;
