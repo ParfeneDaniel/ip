3b714260ce6664695f9affc0a302c94e3687c63e
Parfene Daniel <parfenedaniel345@gmail.com>
2024-09-12 18:53:50 +0300

    new

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const express = require("express");
-const rateLimit = require("express-rate-limit");
-
-const limiter = rateLimit({
-  windowMs: 2 * 60 * 1000,
-  limit: 5,
-});
+const redis = require("redis");
+const { RateLimiterRedis } = require("rate-limiter-flexible");
 
 const app = express();
-app.use(express.json());
-app.use(limiter);
 
-app.get("/", (req, res) => {
+const client = redis.createClient();
+client.connect();
+client.on("connect", () => {
+  console.log("Connected to Redis");
+});
+
+const limiter = new RateLimiterRedis({
+  storeClient: client,
+  keyPrefix: "limiter_by_ip",
+  points: 5,
+  duration: 60,
+  blockDuration: 60,
+});
+
+app.get("/", async (req, res) => {
   try {
     const ip =
       req.headers["cf-connecting-ip"] ||
@@ -19,9 +27,23 @@ app.get("/", (req, res) => {
       req.socket.remoteAddress ||
       "";
 
-    return res.status(200).json({ ip });
-  } catch (error) {
-    return res.status(500).json({ message: "Internal Server Error" });
+    const resIP = await limiter.get(ip);
+
+    if (resIP !== null && resIP.consumedPoints > 5) {
+      const sec = Math.round(resIP.msBeforeNext / 1000) || 1;
+      return res.status(429).json({ message: "To many requests", sec });
+    }
+
+    await limiter.consume(ip);
+
+    return res.status(200).json({ message: "OK" });
+  } catch (rlRejected) {
+    if (rlRejected instanceof Error) {
+      return res.status(500).json({ message: "Internal Server Error" });
+    } else {
+      const sec = Math.round(rlRejected.msBeforeNext / 1000) || 1;
+      return res.status(429).json({ message: "Too Many Requests", sec });
+    }
   }
 });
 
